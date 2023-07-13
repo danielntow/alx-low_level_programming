14089ee0747d9c806de37f23b974f5e47cc3832c
danielntow <danielntow@gmail.com>
2023-07-13 12:14:03 -0700

    assignment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-int main(void)
-
 /**
  * main - calcutling the size of the various variables in bytee
  *
  * Return: Always 0 (Success)
  */
 
+int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
