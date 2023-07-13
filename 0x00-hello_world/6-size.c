20a97d0e5970811220cc5b5540f707f27b3995a8
danielntow <danielntow@gmail.com>
2023-07-13 12:03:00 -0700

    assignment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,5 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-printf("char: %lu\n", sizeof(char));
-printf("short: %lu\n", sizeof(short));
-printf("int: %lu\n", sizeof(int));
-printf("long: %lu\n", sizeof(long));
-printf("long long: %lu\n", sizeof(long long));
-printf("float: %lu\n", sizeof(float));
-printf("double: %lu\n", sizeof(double));
-printf("long double: %lu\n", sizeof(long double));
-
-
 return (0);
 }
