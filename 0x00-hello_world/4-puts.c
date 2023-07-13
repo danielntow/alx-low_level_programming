d3ca2cb2a12120849d1c1f9879203fa36423fa11
danielntow <danielntow@gmail.com>
2023-07-13 09:26:29 -0700

    assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
