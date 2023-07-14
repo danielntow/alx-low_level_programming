dd9a794e457250a2c77d7b3fb81098e447d7f0dd
danielntow <danielntow@gmail.com>
2023-07-14 21:51:54 +0000

    if else

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,22 +16,19 @@ int a = 0;
 int b = 1;
 int c = 2;
 
-while (a < 8)
-{
-	while (b < 9)
-	{
-		while (c < 10)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(c + '0');
-			
-			if (a != 7 || b != 8 || c != 9)
-			{
-				putchar(',');
-				putchar(' ');
+while (a <= 7) {
+	while (b <= 8) {
+		while (c <= 9) {
+			if (a != b && b != c && a != c) {
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+
+				if (!(a == 7 && b == 8 && c == 9)) {
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			
 			c++;
 		}
 		b++;
