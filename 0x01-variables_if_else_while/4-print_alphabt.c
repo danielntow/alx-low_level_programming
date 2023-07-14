99a88da8d756fd25a6412c42abc4fec95d481ce1
danielntow <danielntow@gmail.com>
2023-07-14 17:36:12 +0000

    if else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: This function prints a message using the puts function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++) {
+	if (ch == 'q' || ch == 'e') {
+		continue;
+	}
+
+	putchar(ch);
+}
+
+putchar('\n');
+return (0);
+}
