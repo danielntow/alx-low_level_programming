2c937727d2e49c4a10d88542bb419e459bb34e7a
danielntow <danielntow@gmail.com>
2023-07-14 17:51:06 +0000

    if else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Description: This function prints a message using the puts function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void) {
+int a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+putchar('\n');
+return (0);
+}
