bf8f01214e0c578bdcb6091929d9d8fcf43e6a4a
danielntow <danielntow@gmail.com>
2023-07-14 17:05:59 +0000

    if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: This function prints a message using the puts function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char start = 'a';
+char end = 'z';
+
+for (char ch = start; ch <= end; ch++)
+{
+	putchar(ch);
+	putchar('\n');
+}
+
+return (0);
+}
