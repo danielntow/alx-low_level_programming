6381f46ba2faaed18ca328955f246dfb33852cc9
danielntow <danielntow@gmail.com>
2023-07-15 00:32:51 +0000

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,23 +12,29 @@
 
 int main(void)
 {
-int i = 0;
-int j = 0;
 
-while (i <= 99) {
-	j = i + 1;
-	while (j <= 99) {
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		putchar(' ');
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
-		if (i != 98 || j != 99) {
-			putchar(',');
-			putchar(' ');
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
 		}
-		j++;
 	}
-	i++;
-}return (0);
+	putchar('\n');
+return (0);
 }
