106c74172402dd14c3523d1d57fccfb88cabef30
danielntow <danielntow@gmail.com>
2023-07-18 20:50:47 +0000

    c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,43 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Prints the first 50 Fibonacci numbers
-*
-* Return: Always 0
+* print_fibonacci - Prints the first 50 Fibonacci numbers
 */
-int main(void)
+void print_fibonacci(void)
 {
 int i;
-unsigned long long int fib1 = 1, fib2 = 2, fib;
+unsigned int fib1 = 1, fib2 = 2, fib;
 
-printf("%llu, %llu", fib1, fib2);
+_putchar('1');
+_putchar(',');
+_putchar(' ');
+_putchar('2');
 
 for (i = 3; i <= 50; i++)
 {
 	fib = fib1 + fib2;
-	printf(", %llu", fib);
+
+	_putchar(',');
+	_putchar(' ');
+
+	print_number(fib);
 
 	fib1 = fib2;
 	fib2 = fib;
 }
 
-printf("\n");
+_putchar('\n');
+}
 
-return 0;
+/**
+* print_number - Prints a number using _putchar
+* @n: The number to be printed
+*/
+void print_number(unsigned int n)
+{
+if (n / 10)
+	print_number(n / 10);
+
+_putchar((n % 10) + '0');
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void print_number(unsigned int n)
 #endif /* MAIN_H */
 
