821e35f69a915006efab6f09b0f7b5c027237a1c
danielntow <danielntow@gmail.com>
2023-07-18 17:16:49 +0000

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,16 @@
 #include "main.h"
-// #include <stdio.h>
+#include <stdio.h>
 /**
- *print_alphabet-function begins
- *
- *Return:function ends
- */
-void print_alphabet(void) {
-  //   char alp[28] = "abcdefghijklmnopqrstuvwxyz\n";
-  //   int i = 0;
-
-  //   while (i < 27) {
-  //     _putchar(alp[i]);
-  //     i++;
-  //   }
-  int i;
-  for (i = 'a'; i <= 'z'; i++) {
-    _putchar(i);
-  }
-  _putchar('\n');
+*print_alphabet-function begins
+*
+*Return:function ends
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
