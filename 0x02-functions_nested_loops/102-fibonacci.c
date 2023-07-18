1110f71bf05e5cfe9aa71ccc0cff3ffb2a823d1e
danielntow <danielntow@gmail.com>
2023-07-18 22:08:19 +0000

    nested loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 50 Fibonacci numbers
- *
- * Return: Always 0
- */
+* main - Prints the first 50 Fibonacci numbers
+*
+* Return: Always 0
+*/
 int main(void)
 {
-    int i;
-    int fib[50];
+int i;
+int fib[50];
 
-    fib[0] = 1;
-    fib[1] = 2;
+fib[0] = 0;
+fib[1] = 1;
 
-    for (i = 2; i < 50; i++)
-    {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    printf("%d", fib[0]);
-
-    for (i = 1; i < 50; i++)
-    {
-        printf(", %d", fib[i]);
-    }
-
-    printf("\n");
-
-    return 0;
+for (i = 2; i < 50; i++)
+{
+	fib[i] = fib[i - 1] + fib[i - 2];
 }
 
+printf("%d", fib[0]);
 
+for (i = 1; i < 50; i++)
+{
+	printf(", %d", fib[i]);
+}
+
+printf("\n");
+
+return (0);
+}
