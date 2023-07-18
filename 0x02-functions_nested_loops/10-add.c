62dbdf725b0fa4338a9ea4a4ca65635d89f45a4c
danielntow <danielntow@gmail.com>
2023-07-18 20:18:16 +0000

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+* Adds two integers and returns the result.
+*
+* @param a The first integer to add.
+* @param b The second integer to add.
+* @return The sum of a and b.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
