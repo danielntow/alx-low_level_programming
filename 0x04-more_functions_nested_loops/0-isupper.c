4e8084dcd7c3b4098c53600f1481a5d2b3c6ce94
danielntow <danielntow@gmail.com>
2023-07-20 12:41:30 +0000

    debugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+* _islower - Checks if a character is lowercase
+* @c: The character to be checked
+*
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+
+}
