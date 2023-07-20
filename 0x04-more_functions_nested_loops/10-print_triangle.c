1d1976ace899ca0024c2ad287e278d92bc0a16b6
danielntow <danielntow@gmail.com>
2023-07-20 22:20:37 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+*print_traingle - check the code
+*@size: to dermine the triangle
+*Return: 0
+*/
+
+void print_triangle(int size)
+{
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = size - i - 1; k > 0; k--)
+		{
+			putchar(' ');
+		}
+
+		for (j = 0; j <= i; j++)
+		{
+
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
