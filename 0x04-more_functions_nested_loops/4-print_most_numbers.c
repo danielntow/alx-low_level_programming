45de9127d5a09e130ed765fa54fbe39bfed9ab3d
danielntow <danielntow@gmail.com>
2023-07-20 13:41:13 +0000

    assignment

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers from 0-9 but skips 4 and 5
+ *
+ */
+
+void print_most_numbers(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i == 4 || i == 5)
+            continue;
+        putchar(i + '0');
+    }
+    putchar('\n');
+}
