881e4a713d7366d7e6660deab3fae68304142cf7
danielntow <danielntow@gmail.com>
2023-07-20 13:46:54 +0000

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * print_most_numbers - prints numbers from 0-9 but skips 4 and 5
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
  */
 
 void print_most_numbers(void)
