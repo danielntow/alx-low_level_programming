7b104d9c8a480da6f09b09d99040bb5e356e11a3
danielntow <danielntow@gmail.com>
2023-07-20 14:50:38 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line when it encounter a char
+ *
+ * @n: a number that determines the number of x a straight line is drawn
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			putchar('-');
+	}
+	else
+	{
+		putchar('\n');
+	}
+	putchar('\n');
+}
