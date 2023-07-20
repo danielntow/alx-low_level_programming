f56d16e9f2b670c572b25c1715cacbabdb3a2640
danielntow <danielntow@gmail.com>
2023-07-20 17:51:55 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0 && i != 0)
+			printf("%s ", "FizzBuss");
+		if (i % 3 == 0 && i != 0)
+			printf("%s", "Fizz ");
+		else if (i % 5 == 0 && i != 0)
+			printf("%s ", "Buzz");
+		else if (i % 3 == 0 && i % 5 == 0 && i != 0)
+			printf("%s ", "FizzBuss");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
