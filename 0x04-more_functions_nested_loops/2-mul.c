708097b4aac6a38be2458fc6e1f1f313821ae146
danielntow <danielntow@gmail.com>
2023-07-20 13:01:29 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - function to find the product of 2 numbers
+ * @a: the int to be multiplied
+ * @b: the other int
+ * Return: product of @a and @b
+ */
+
+int mul(int a, int b)
+{
+	int product;
+	product = a * b;
+	return (product);
+}
