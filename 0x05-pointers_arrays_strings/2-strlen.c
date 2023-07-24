19d319e7185c989020d07feace6511d3a39f73e6
danielntow <danielntow@gmail.com>
2023-07-24 14:51:54 +0000

    more functions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the input string.
+ *
+ * Return: The length of the string (number of characters).
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
