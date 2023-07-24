f208d472cd32186cd206785af5b35bca49c93308
danielntow <danielntow@gmail.com>
2023-07-24 23:42:58 +0000

    more functions

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@
 
 void puts_half(char *str)
 {
+int length = strlen(str);
 
-	int length = strlen(str);
-	int half_length = (length) / 2;
+	int half_length = (length - 1) / 2;
 	int i;
 
-	for (i = half_length; i < length; i++)
+	for (i = half_length + 1; i < length; i++)
 	{
 		putchar(str[i]);
 	}
