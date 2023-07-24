59a429a6b136d3eeb3b20cfc9ed28f9848d5cc79
danielntow <danielntow@gmail.com>
2023-07-24 22:57:30 +0000

    more functions

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - check the code
+ * @str: Pointer to the input string.
+ */
+
+void puts_half(char *str)
+{
+
+	int length = strlen(str);
+	int half_length = length / 2;
+	int i;
+
+	for (i = half_length; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	if (length % 2 != 0)
+		putchar(str[length - 1]);
+	putchar('\n');
+}
