7b4f39c697bfdfc3c411be2e32e7d6edfc1bd2ad
danielntow <danielntow@gmail.com>
2023-07-24 18:31:43 +0000

    more functions

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverses the input string in place.
+ * @s: Pointer to the input string to be reversed.
+ */
+
+void rev_string(char* s)
+
+{
+	int length = strlen(s);
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		*s = (s[i]);
+	}
+
+	putchar('\n');
+}
+
