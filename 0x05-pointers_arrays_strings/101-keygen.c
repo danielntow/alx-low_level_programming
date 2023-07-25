a52df2ef796b6920f0259aa1e6fb77a0bccf841f
danielntow <danielntow@gmail.com>
2023-07-25 10:03:18 +0000

    more functions

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 /**
- * main - check the code
- *
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
 
 	sum = 0;
+
 	srand(time(NULL));
+
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
@@ -30,5 +30,11 @@ int main(void)
 			break;
 		}
 	}
+
 	return (0);
 }
+
+
+
+
+
