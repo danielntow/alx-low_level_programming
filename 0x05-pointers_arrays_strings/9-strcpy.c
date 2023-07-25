1b3f44d3c0b0b5f7b90322cd0ead85ee893ae4fa
danielntow <danielntow@gmail.com>
2023-07-25 00:36:27 +0000

    more functions

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *_strcpy - check the code
+ *
+ *@dest: a pointer variable
+ *@src: another pointer variable
+ *
+ *Return: a pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
