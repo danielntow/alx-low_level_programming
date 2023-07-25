4cd088d6a650944de255900fee97c4dc898f2d9e
danielntow <danielntow@gmail.com>
2023-07-25 00:39:25 +0000

    more functions

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_atoi - check the code
+ *
+ *@s: a pointer variable
+ *Return: an integer
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
