0f280d6c9298134f86768bff28ce3c35ab949a82
danielntow <danielntow@gmail.com>
2023-07-26 10:53:00 +0000

    more functions

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,22 +13,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	size_t dest_size = sizeof(dest);
 	size_t dest_len = strlen(dest);
-	size_t src_len = strlen(src);
-
-	// printf("Destination size: %zu\n", dest_size);
-	// printf("Destination length: %zu\n", dest_len);
-	// printf("Source length: %zu\n", src_len);
 
 	while (src[i] != '\0')
 	{
 		dest[dest_len + i] = src[i];
 		i++;
 	}
-	dest[dest_len + i] = '\0'; // Null-terminate the concatenated string
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 
 }
-
