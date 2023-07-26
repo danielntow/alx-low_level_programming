d6cf6be299af87f7839a057a568cd5e13ee6c86c
danielntow <danielntow@gmail.com>
2023-07-26 21:51:57 +0000

    more functions

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,35 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
- * string_toupper - Changes all lowercase letters of a string to uppercase.
- * @str: The input string.
+ * cap_string - Capitalizes all words in a string
+ * @str: Pointer to the input string to be modified.
  *
- * Return: A pointer to the modified string.
+ * Return: Pointer to the modified string.
  */
 
-char *string_toupper(char *str)
+
+char *cap_string(char *str)
 {
-	int i, length;
-
-	char start = 'a';
-	char end = 'z';
-
-	length = strlen(str);
-
-	for (i = 0; i < length; i++)
+	int capitalize_next =
+	    1;
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= start && str[i] <= end)
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}')
 		{
-			str[i] -= 32;
+			capitalize_next = 1;
+		}
+		else
+		{
+			if (capitalize_next && islower(str[i]))
+			{
+				str[i] = toupper(str[i]);
+				capitalize_next = 0;
+			}
 		}
 	}
 
