e4566406d4b99e254da043df18b19a81616a7f64
danielntow <danielntow@gmail.com>
2023-07-27 10:14:12 +0000

    more functions

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,35 +3,33 @@
 
 /**
  * print_number - Prints an integer to the standard output using _putchar.
- * @n: The integer to be printed.
+ * @num: The integer to be printed.
  *
  * Return: void.
  */
 
-void print_number(int n)
+void print_number(int num)
 {
-	int divisor = 1;
-	int num;
-
-	if (n < 0)
+	if (num < 0)
 	{
 		putchar('-');
-		n = -n;
+		num = -num;
 	}
 
-	num = n;
-	while (num >= 10)
-	{
-		divisor *= 10;
-		num /= 10;
-	}
+	int thousands_digit = num / 1000;
+	int hundreds_digit = (num / 100) % 10;
+	int tens_digit = (num / 10) % 10;
+	int ones_digit = num % 10;
 
-	num = n;
-	while (divisor != 0)
-	{
-		putchar('0' + num / divisor);
-		num %= divisor;
-		divisor /= 10;
-	}
+	if (thousands_digit != 0)
+		putchar('0' + thousands_digit);
+
+	if (hundreds_digit != 0 || thousands_digit != 0)
+		putchar('0' + hundreds_digit);
+
+	if (tens_digit != 0 || hundreds_digit != 0 || thousands_digit != 0)
+		putchar('0' + tens_digit);
+
+	putchar('0' + ones_digit);
 }
 
