f8ff4bb70a96ee49772121b271a8f5a9ab8fafb7
danielntow <danielntow@gmail.com>
2023-07-28 00:52:14 +0000

    more functions

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,43 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
 /**
- * leet - Encodes a string into 1337.
- * @str: The input string.
- *
- * Return: A pointer to the modified string.
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-
-char *leet(char *str)
+char *leet(char *n)
 {
-	int i;
-	char *leet_chars = "43071";
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = leet_chars[0];
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = leet_chars[1];
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = leet_chars[2];
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = leet_chars[3];
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = leet_chars[4];
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 		}
 	}
-
-	return (str);
+	return (n);
 }
