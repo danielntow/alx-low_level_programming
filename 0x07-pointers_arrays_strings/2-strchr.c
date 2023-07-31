64fb9b626b85720f6b1933ffffc7657786b11ba6
danielntow <danielntow@gmail.com>
2023-07-31 13:44:42 +0000

    more functions

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	for (int i = 0; s[i] != '\0'; i++)
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
 	}
-
 	return (0);
 }
