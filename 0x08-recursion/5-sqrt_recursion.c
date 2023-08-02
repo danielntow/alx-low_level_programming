e7027a913ee7373ac6b9ea5fe4a4d720ca717515
danielntow <danielntow@gmail.com>
2023-08-02 15:48:24 +0000

    more functions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,27 +12,20 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 		return (n);
-
-	int start = 1;
-	int  end = n;
-	 int ans;
-
-	while (start <= end)
+	else
 	{
-		int mid = (start + end) / 2;
+		int guess = 1;
 
-		if (mid * mid == n)
-			return (mid);
-		if (mid * mid < n)
+		while (guess * guess <= n)
 		{
-			start = mid + 1;
-			ans = mid;
-		}
-		else
-			end = mid - 1;
-	}
-	return (ans);
-}
+			if (guess * guess == n)
+				return (guess);
 
+			guess++;
+		}
+
+		return (-1);
+	}
+}
