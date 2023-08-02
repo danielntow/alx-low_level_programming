c1e010a2416ffec64360b4ad6a5a43172b2a7aa9
danielntow <danielntow@gmail.com>
2023-08-02 13:18:33 +0000

    more functions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * _puts_recursion - Prints a string, followed by a new line, using recursion.
  * @s: The input string to be printed.
  */
-void _puts_recursion(char* s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
