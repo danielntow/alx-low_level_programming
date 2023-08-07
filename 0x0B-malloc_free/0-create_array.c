decb4fc3b81783a86e0023bc17da643df5fb8d4a
danielntow <danielntow@gmail.com>
2023-08-07 13:07:14 +0000

    more functions

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
@@ -27,7 +29,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
