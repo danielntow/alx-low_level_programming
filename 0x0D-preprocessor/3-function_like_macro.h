d23f8fa78ec2db10d82a3086e7c28255039706c1
danielntow <danielntow@gmail.com>
2023-08-14 13:45:44 +0000

    more functions

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,14 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x)                                                                 \
-	do                                                                     \
-	{                                                                      \
-		if ((x) < 0)                                                   \
-		{                                                              \
-			(x) = -(x);                                            \
-		}                                                              \
-	} while (0)
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
 
