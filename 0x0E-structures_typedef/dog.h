d48162f94a35c15943517a6e2c7aa9c657a626b9
danielntow <danielntow@gmail.com>
2023-08-14 15:32:41 +0000

    more functions

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure defines a dog's information including its name,
+ * age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
