0017598f85aafb5b67a6e5666a6891cd055d0850
danielntow <danielntow@gmail.com>
2023-08-18 05:15:56 +0000

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,7 +22,9 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		int arg = va_arg(args, int);
+		int arg;
+
+		arg = va_arg(args, int);
 
 		sum += arg; /* Add the current number to the sum */
 	}
