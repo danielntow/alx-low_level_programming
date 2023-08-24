f9f13253457bc99e329b33447d4b0e2d97ddf4f5
danielntow <danielntow@gmail.com>
2023-08-24 20:49:45 +0000

    linked list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,11 @@
 section .data
-    hello db "Hello, Holberton", 0
-
+    hello db 'Hello, Holberton', 0
 section .text
-    global main
-
-extern printf
-main:
-    mov rdi, hello
+    global _start
+_start:
+    push hello
     call printf
-
+    add rsp, 8
+    mov eax, 0
     ret
 
