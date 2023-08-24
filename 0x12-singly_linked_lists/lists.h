cac55b7a5c530d331fd5fc9373b2a8719082790f
danielntow <danielntow@gmail.com>
2023-08-24 14:30:07 +0000

    linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,8 @@ typedef struct list_s
 /* Function to print all elements of a list */
 size_t print_list(const list_t *h);
 
+/* Function to get the number of elements in a list */
+size_t list_len(const list_t *h);
+
 #endif /* LIST_H */
 
