b67d2490fd2c7b29d74e961c7c2b1ecac065e5bb
danielntow <danielntow@gmail.com>
2023-08-24 22:04:49 +0000

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,18 @@
  */
 size_t print_list(const list_t *h)
 {
-
 	size_t count = 0;
 
 	while (h != NULL)
 	{
+		if (h != NULL && h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
 	return (count);
 }
-
