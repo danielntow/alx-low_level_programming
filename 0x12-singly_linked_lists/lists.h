adaaeaf692af1bcdac531108e900ee43816c9b4a
danielntow <danielntow@gmail.com>
2023-08-24 14:36:01 +0000

    linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,5 +27,8 @@ size_t print_list(const list_t *h);
 /* Function to get the number of elements in a list */
 size_t list_len(const list_t *h);
 
+/* Function to add a new node at the beginning of a list */
+list_t *add_node(list_t **head, const char *str);
+
 #endif /* LIST_H */
 
