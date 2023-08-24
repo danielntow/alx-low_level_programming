34a334e7517b6ec345f63f424f612dff2258920b
danielntow <danielntow@gmail.com>
2023-08-24 15:42:35 +0000

    linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
@@ -30,5 +31,25 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new_node;
 
 	return (new_node);
+}
+
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
 
