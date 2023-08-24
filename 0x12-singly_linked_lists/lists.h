540e5768169ef19b5d6cb3e4dbe89398692bba6c
danielntow <danielntow@gmail.com>
2023-08-24 16:20:35 +0000

    linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,5 +30,8 @@ size_t list_len(const list_t *h);
 /* Function to add a new node at the beginning of a list */
 list_t *add_node(list_t **head, const char *str);
 
+/* Function to add a new node at the end of a list */
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif /* LIST_H */
 
