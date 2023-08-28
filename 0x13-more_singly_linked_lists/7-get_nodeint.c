be3322c8fc393cec992c3169924bf631cce9e7ab
danielntow <danielntow@gmail.com>
2023-08-28 18:58:54 +0000

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,32 +12,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current;
+	listint_t *current = head;
+	unsigned int count = 0;
 
-	size_t count;
-
-	count = 0;
-
-	current = head;
-
-	if (head == NULL || index < 0)
-		return (NULL);
-
-	while (current && count <= index)
+	while (current != NULL)
 	{
 		if (count == index)
-		{
-			return (current);
-		}
+			return (current); /* Return the node at the given index */
 
-		else
-		{
-
-			current = current->next;
-			count++;
-		}
+		current = current->next;
+		count++;
 	}
 
-	return (NULL);
+	return (NULL); /* Return NULL if the node does not exist */
 }
 
