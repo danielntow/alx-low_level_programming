b27a6d61a3ecaf66387de0c426b801640cf6c1a9
danielntow <danielntow@gmail.com>
2023-08-28 19:05:26 +0000

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -31,15 +31,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		{
 			return (current);
 		}
-
 		else
 		{
-
 			current = current->next;
 			count++;
 		}
 	}
-
 	return (NULL);
 }
 
