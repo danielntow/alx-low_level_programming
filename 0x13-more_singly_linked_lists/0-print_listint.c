de167604497108a1a0be832cbd7e36e7f44cf9e6
danielntow <danielntow@gmail.com>
2023-08-29 12:44:20 +0000

    linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,46 +5,22 @@
 
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the list.
- * Return: The number of nodes in the list.
- */
+* print_listint - Prints all the elements of a listint_t list.
+* @h: Pointer to the head of the list.
+* Return: The number of nodes in the list.
+*/
 size_t print_listint(const listint_t *h)
 {
+const listint_t *current = h;
+size_t count = 0;
 
-	const listint_t *current = h;
-	size_t count = 0;
-	int num;
-
-	while (current)
-	{
-		num = current->n;
-
-		/* Handle the case of -ve numbers */
-		if (num < 0)
-		{
-			putchar('-');
-			num = -num;
-		}
-		/* Print the number */
-		print_number(num);
-		/* Print a newline after each number */
-		putchar('\n');
-
-		current = current->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * print_number - Prints a number using putchar.
- * @n: The number to be printed.
- */
-void print_number(int n)
+while (current != NULL)
 {
-	if (n / 10 != 0)
-		print_number(n / 10);
-
-	putchar(n % 10 + '0');
+printf("%d\n", current->n);
+current = current->next;
+count++;
 }
+
+return (count);
+}
+
