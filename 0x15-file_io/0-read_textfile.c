4a60ac7c6083b5dc9fb76d256f11ac523aec7509
danielntow <danielntow@gmail.com>
2023-09-04 22:01:10 +0000

    linked list

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads and prints the contents of a text file.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, 0 on error.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	ssize_t bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1 || bytes_read != (ssize_t)letters)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (bytes_written);
+}
