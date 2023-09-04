4ee801a9372a5baeee117932dceb49e4dc350180
danielntow <danielntow@gmail.com>
2023-09-04 22:33:47 +0000

    file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *allocate_buffer(char *filename);
+void close_descriptor(int descriptor);
 #endif /* MAIN_H */
