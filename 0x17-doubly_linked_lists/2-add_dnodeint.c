b37269e8f665fec7b18b1ca4f7a0f61fd2d3a4f2
danielntow <danielntow@gmail.com>
2023-10-12 15:45:08 +0000

    doubly linked list in c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,31 +4,22 @@
 #include "lists.h"
 
 /**
- * create_dlistint_node - Create a new node for a doubly linked list
- * @n: The value to assign to the new node
- * Return: A pointer to the new node, or NULL on failure
- */
-dlistint_t *create_dlistint_node(int n)
+* add_dnodeint - Add a new node at the beginning of a doubly linked list
+* @head: A pointer to a pointer to the head of the list
+* @n: The value to add to the new node
+* Return: The address of the new element, or NULL if it failed
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 if (newNode == NULL)
-	return (NULL);
-
-newNode->n = n;
-return (newNode);
+{
+return (NULL);
 }
 
-/**
- * add_dnodeint - Add a new node at the beginning of a doubly linked list
- * @head: A pointer to a pointer to the head of the list
- * @n: The value to add to the new node
- * Return: The address of the new element, or NULL if it failed
- */
-
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-dlistint_t *newNode = create_dlistint_node(n);
+newNode->n = n;
+newNode->next = NULL;
 
 if (*head == NULL)
 {
