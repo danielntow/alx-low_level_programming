a0ca18c2a86ee8c4583eb8ec6be386f66812d399
danielntow <danielntow@gmail.com>
2023-10-12 14:31:53 +0000

    doubly linked list in c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,18 +3,36 @@
 #include <string.h>
 #include "lists.h"
 
+dlistint_t *create_dlistint_node(int n)
+{
+    dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+    if (newNode == NULL)
+    {
+        return (NULL);
+    }
+
+    newNode->n = n;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    return (newNode);
+}
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
     dlistint_t *newNode = create_dlistint_node(n);
 
     if (*head == NULL)
+    {
         *head = newNode;
-
-    (*head)->next = newNode;
-    newNode->next = (*head)->next;
-    newNode->prev = *head;
-    if ((*head)->next != NULL)
-        (*head)->next->prev = newNode;
+    }
+    else
+    {
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
+    }
 
     return (*head);
 }
