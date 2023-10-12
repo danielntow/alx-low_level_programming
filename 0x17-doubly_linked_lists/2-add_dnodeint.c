91f47daf9bfa0816a145fd1a35a1115070580737
danielntow <danielntow@gmail.com>
2023-10-12 14:33:42 +0000

    doubly linked list in c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,34 +5,34 @@
 
 dlistint_t *create_dlistint_node(int n)
 {
-    dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
 
-    if (newNode == NULL)
-    {
-        return (NULL);
-    }
+if (newNode == NULL)
+{
+return (NULL);
+}
 
-    newNode->n = n;
-    newNode->prev = NULL;
-    newNode->next = NULL;
+newNode->n = n;
+newNode->prev = NULL;
+newNode->next = NULL;
 
-    return (newNode);
+return (newNode);
 }
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *newNode = create_dlistint_node(n);
+dlistint_t *newNode = create_dlistint_node(n);
 
-    if (*head == NULL)
-    {
-        *head = newNode;
-    }
-    else
-    {
-        newNode->next = *head;
-        (*head)->prev = newNode;
-        *head = newNode;
-    }
+if (*head == NULL)
+{
+*head = newNode;
+}
+else
+{
+newNode->next = *head;
+(*head)->prev = newNode;
+*head = newNode;
+}
 
-    return (*head);
+return (*head);
 }
