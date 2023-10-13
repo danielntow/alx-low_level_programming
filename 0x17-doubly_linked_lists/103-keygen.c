cb86f3001cd89805f2603adf5a422792513c4486
danielntow <danielntow@gmail.com>
2023-10-13 10:24:57 +0000

    doubly linked list in c

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    char *username = argv[1];
+    uint32_t key = 0;
+
+    int i;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <username>\n", argv[0]);
+        return 1;
+    }
+
+    for (i = 0; username[i] != '\0'; i++)
+    {
+        key += username[i];
+    }
+
+    printf("Valid key for username %s: %u\n", username, key);
+    return 0;
+}
