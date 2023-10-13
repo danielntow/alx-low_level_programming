a1410c140a0cac80c48be86f3592bd64c0efc5a4
danielntow <danielntow@gmail.com>
2023-10-13 10:30:34 +0000

    doubly linked list in c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,24 +2,32 @@
 #include <string.h>
 #include <stdint.h>
 
+/**
+ * main - Entry point for the program.
+ *
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the program's arguments.
+ *
+ * Return: Always returns 0 (success).
+ */
 int main(int argc, char *argv[])
 {
-    char *username = argv[1];
-    uint32_t key = 0;
+char *username = argv[1];
+uint32_t key = 0;
 
-    int i;
+int i;
 
-    if (argc != 2)
-    {
-        printf("Usage: %s <username>\n", argv[0]);
-        return 1;
-    }
+if (argc != 2)
+{
+printf("Usage: %s <username>\n", argv[0]);
+return (1);
+}
 
-    for (i = 0; username[i] != '\0'; i++)
-    {
-        key += username[i];
-    }
+for (i = 0; username[i] != '\0'; i++)
+{
+key += username[i];
+}
 
-    printf("Valid key for username %s: %u\n", username, key);
-    return 0;
+printf("Valid key for username %s: %u\n", username, key);
+return (0);
 }
