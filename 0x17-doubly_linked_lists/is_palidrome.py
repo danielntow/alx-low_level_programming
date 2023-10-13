8c5355dea0dd885e2de618b4372935b3d9ff5547
danielntow <danielntow@gmail.com>
2023-10-13 05:29:45 +0000

    doubly linked list in c

---
+++ 0x17-doubly_linked_lists/is_palidrome.py
@@ -0,0 +1,46 @@
+# Function to check if a number is a palindrome
+import os
+
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+
+largest_palindrome = 0
+
+# Loop through all 3-digit numbers
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result to a file (without newline or extra space)
+with open("100-result", "w") as file:
+    file.write(str(largest_palindrome))
+    print(str(largest_palindrome))
+
+
+# Get the current directory
+current_directory = os.path.dirname(os.path.abspath(__file__))
+result_file_path = os.path.join(current_directory, "102-result")
+
+# Function to check if a number is a palindrome
+
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+
+largest_palindrome = 0
+
+# Loop through all 3-digit numbers
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result to the "102-result" file in the current directory
+with open(result_file_path, "w") as file:
+    file.write(str(largest_palindrome))
