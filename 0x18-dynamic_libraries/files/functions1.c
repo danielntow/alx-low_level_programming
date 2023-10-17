a730537ea271215fcf29fe010b9db88dcb93c862
danielntow <danielntow@gmail.com>
2023-10-17 00:21:09 +0000

    dynamic libraries in  c

---
+++ 0x18-dynamic_libraries/files/functions1.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int _putchar(char c) {
+    /* Empty implementation for _putchar */
+    return (0);
+}
+
+int _islower(int c) {
+    /* Empty implementation for _islower */
+    return (0);
+}
+
+int _isalpha(int c) {
+    /* Empty implementation for _isalpha */
+    return (0);
+}
+
+int _abs(int n) {
+    /* Empty implementation for _abs */
+    return (0);
+}
+
+int _isupper(int c) {
+    /* Empty implementation for _isupper */
+    return (0);
+}
---
+++ 0x18-dynamic_libraries/files/functions2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+int _isdigit(int c)
+{
+    /* Empty implementation for _isdigit */
+    return (0);
+}
+
+int _strlen(char *s)
+{
+    /* Empty implementation for _strlen */
+    return (0);
+}
+
+void _puts(char *s)
+{
+    /* Empty implementation for _puts */
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    /* Empty implementation for _strcpy */
+    return (NULL);
+}
+
+int _atoi(char *s)
+{
+    /* Empty implementation for _atoi */
+    return (0);
+}
