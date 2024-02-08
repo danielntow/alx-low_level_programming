6792306fb625c3a03d9141f49615b370ca2cbf7e
danielntow <danielntow@gmail.com>
2024-02-08 13:52:34 +0000

    jump search linked_lisst

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,4 @@
-ifndef SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 /*headers*/
 #include <stdio.h>
@@ -32,5 +32,6 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 int my_binary_search(int *array, int low, int high, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
